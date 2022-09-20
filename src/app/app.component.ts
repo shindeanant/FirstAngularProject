cf51bc87986d8f10831828f882955ba8a47ced2c
shindeanant <shindeanant1997@gmail.com>
2022-09-20 21:35:50 +0530

    Added:Ability to lanuch bridgelabz site in a new tab on clicking BridgeLabz Logo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Helloworld';
   imgUrl="../assets/BL_Logo.jpeg"
+  url="https://www.bridgelabz.com"
 
   ngOnInit(): void{
     this.title = "Hello from BridgeLabz. "
+  }
+
+  onClick($event: any){
+    console.log("Save button is clicked!", $event)
+    window.open(this.url, "_blank");
+    
   }
 
 }
